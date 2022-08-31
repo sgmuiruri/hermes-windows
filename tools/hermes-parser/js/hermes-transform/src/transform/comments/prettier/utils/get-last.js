3fb929e38ea41e90c3a4a482b95e64502a3930f9
Vladimir Morozov <vmorozov@microsoft.com>
2022-08-30 21:03:57 -0700

    Synchronize files previously incorrectly merged

--- tools/hermes-parser/js/hermes-transform/src/transform/comments/prettier/utils/get-last.js
+++ tools/hermes-parser/js/hermes-transform/src/transform/comments/prettier/utils/get-last.js
@@ -1,11 +1,14 @@
-/*
+/**
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
 
-#[macro_use]
-mod ast;
+'use strict';
 
-mod gen_js;
+const getLast = arr => arr[arr.length - 1];
+
+module.exports = getLast;
